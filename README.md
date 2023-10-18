35ccf5b1910113a37d9aac1628104c3d22210f3e
Cri <146863778+cris-programming@users.noreply.github.com>
2023-10-18 10:30:27 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,60 @@
+# Human Detection using OpenCV
+
+This Python script uses the OpenCV library to detect humans, faces, upper bodies, and lower bodies through a camera feed. It captures frames from the camera, processes them to identify objects, and logs the detection times and saves images when objects are detected.
+
+## Table of Contents
+- [Prerequisites](#prerequisites)
+- [Getting Started](#getting-started)
+- [Usage](#usage)
+- [Configuration](#configuration)
+
+## Prerequisites
+Before running the script, you need to ensure you have the following prerequisites installed on your system:
+- Python 3
+- OpenCV Python Library
+- A webcam or camera connected to your computer
+- XML files from OpenCv GitHub
+
+You can install OpenCV for Python using pip:
+
+```bash
+pip install opencv-python
+```
+
+## Getting Started
+1. Clone this repository to your local machine or download the script directly.
+
+2. Ensure you have the required Haar cascade XML files for human, face, upper body, and lower body detection. You can find these XML files online or use the pre-trained ones. Update the paths to these files in the script:
+
+```python
+human_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+upperbody_cascade = cv2.CascadeClassifier('haarcascade_upperbody.xml')
+lowerbody_cascade = cv2.CascadeClassifier('haarcascade_lowerbody.xml')
+```
+
+3. Run the script:
+
+```bash
+python main.py
+```
+
+## Usage
+- Once the script is running, it will open the camera feed.
+
+- The script continuously captures frames from the camera and processes them for human, face, upper body, and lower body detection.
+
+- If any of these objects are detected, the script will save an image with a timestamp in the format "human_YYYY-MM-DD_HH-MM-SS.jpg".
+
+- It also logs the detection time in a text file named "detection_log.txt".
+
+- To exit the script, press the 'q' key.
+
+## Configuration
+You can configure the following settings in the script:
+
+- `camera`: Set to '0' for the default camera. Change it to a different number if you have multiple cameras.
+
+- `log_interval`: Set the interval (in seconds) at which the script logs detection times and saves images.
+
+- `quit_key`: Set the key you will use to quit the program
---
+++ main.py
@@ -0,0 +1,75 @@
+from cv2 import CascadeClassifier, VideoCapture, cvtColor, COLOR_BGR2GRAY, imwrite, imshow, waitKey, destroyAllWindows
+from datetime import datetime
+quit_key = 'q'
+
+# Load pre-trained human detection models
+human_cascade = CascadeClassifier('haarcascade_fullbody.xml')
+face_cascade = CascadeClassifier('haarcascade_frontalface_default.xml')
+upperbody_cascade = CascadeClassifier('haarcascade_upperbody.xml')
+lowerbody_cascade = CascadeClassifier('haarcascade_lowerbody.xml')
+
+# Initialize the camera
+camera = 0 # 0 for default camera, change to a different number if you have multiple cameras.
+cap = VideoCapture(camera)
+# Check if the camera exist
+if not cap.isOpened():
+    print("Error: Camera not found.")
+    exit(1)
+
+# Initialize variables to control picture and log intervals
+last_capture_time = datetime.now()
+log_interval = 10  # Log an entry and take picture every X seconds
+
+def save_image(timestamp):
+    image_filename = f"human_{timestamp}.jpg"
+    imwrite(image_filename, frame)
+    # Check if is possible to save the img
+    if not imwrite(image_filename, frame):
+        print(f"Error: Unable to save image {image_filename}")
+
+def save_log(timestamp):
+    try:
+        with open('detection_log.txt', 'a') as log_file:
+            log_file.write(f"Human detected at {timestamp}\n")
+    except IOError:
+        print("Error: Unable to write to the log file.")
+
+while True:
+    ret, frame = cap.read()  # Capture a frame from the camera
+
+    if not ret:
+        print("Error: Unable to capture frame.")
+        continue
+
+    # Convert the frame to grayscale for detection
+    gray = cvtColor(frame, COLOR_BGR2GRAY)
+
+    # Detect humans in the frame
+    humans = human_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    
+    # Detect lower and upper bodies
+    upper_bodies = upperbody_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    lower_bodies = lowerbody_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    time_difference = int((datetime.now() - last_capture_time).total_seconds())
+
+    if (len(humans) > 0 or len(faces) > 0 or len(upper_bodies) > 0 or len(lower_bodies) > 0) and time_difference - log_interval > 0:
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        # A human is detected, capture the image
+        save_image(timestamp)
+
+        # Log the detection time to a text file
+        save_log()
+        
+        last_capture_time = datetime.now()
+
+    imshow('Human Detection', frame)
+    
+    if waitKey(1) & 0xFF == ord(quit_key):
+        break
+
+cap.release()
+destroyAllWindows()
